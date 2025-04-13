e9519f8546edb7a7d5097a7a3995d03e6006d3db
Kirill <kingofnothing@bk.ru>
2025-04-13 17:48:22 +0300

    v1.2

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const burger = document.querySelector('.header_item.burger')
   const burgerMenu = document.querySelector('.burger-nav')
+  const links = document.querySelectorAll('.burger-nav li a')
   burger.onclick = () => {
     burgerMenu.classList.toggle('open')
     burger.classList.toggle('active')
   }
+  links.forEach(link => {
+    link.onclick = () => {
+      burgerMenu.classList.toggle('open')
+      burger.classList.toggle('active')
+    }
+  })
 })
