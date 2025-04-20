3360f4b118b5e6ecbea53ed67abcd925ad2b0bd8
Kirill <kingofnothing@bk.ru>
2025-04-20 18:13:05 +0300

    v1.3

---
+++ js/sliders.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (Swiper) {
+    setTimeout(() => {}, 100)
+    const toc_swiper = document.querySelectorAll('.toc .swiper')
+    toc_swiper.forEach(slider => {
+      const swiperInstance = new Swiper(slider, {
+        loop: true,
+        slidesPerView: 2,
+        // spaceBetween: 24,
+        navigation: {
+          nextEl: '.toc .btn-next',
+          prevEl: '.toc .btn-prev',
+        },
+      })
+    })
+    const services_swiper = document.querySelectorAll('#services .swiper')
+    services_swiper.forEach(slider => {
+      const swiperInstance = new Swiper(slider, {
+        slidesPerView: 3,
+        loop: true,
+        navigation: {
+          nextEl: '#services .btn-next',
+          prevEl: '#services .btn-prev',
+        },
+        breakpoints: {
+          360: {
+            slidesPerView: 1,
+          },
+          480: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+        },
+      })
+    })
+  }
+})
